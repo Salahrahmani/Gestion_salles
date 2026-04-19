8df8f35a4b28bb32dff5915bd9bbcc4deadaa030
SALAH <salaheddine.rahmani@monborial.ca>
2026-04-18 22:44:51 -0400

    add Ajouter_salle Valid

--- services/services_salle.py
+++ services/services_salle.py
@@ -3,3 +3,7 @@ from Data.dao_salle import DataSalle
 class ServiceSalle:
     def __init__(self):
         self.dao = DataSalle()
+
+    def ajouter_salle(self, salle):
+        if not all([salle.code, salle.description, salle.categorie, salle.capacite]):
+            return False, "All fields are required."

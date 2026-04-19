ca426240084ea2b9ce22a1ff5f828b2d7abe9103
SALAH <salaheddine.rahmani@monborial.ca>
2026-04-18 22:49:44 -0400

    add modifier_salle meth

--- services/services_salle.py
+++ services/services_salle.py
@@ -11,4 +11,11 @@ class ServiceSalle:
             return False, "invalid capacity"
 
         self.dao.insert_salle(salle)
-        return True, "Salle add"
+        return True, "Salle modified"
+
+    def modifier_salle(self, salle):
+        if salle.capacite < 1:
+            return False, "invalid capacity"
+
+        self.dao.update_salle(salle)
+        return True, "Room modified"

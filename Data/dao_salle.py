6fe4ba00667457a9c783329eea44a1fe067e3b82
SALAH <salaheddine.rahmani@monborial.ca>
2026-04-18 22:05:31 -0400

    DB conn

---
+++ Data/dao_salle.py
@@ -0,0 +1,14 @@
+import mysql.connector
+import json
+class DataSalle:
+    def get_connection(self):
+        with open ("Data/config.json") as f :
+            config = json.load(f)
+
+            conn = mysql.connector.connect(
+                host=config["host"],
+                user=config["user"],
+                password=config["password"],
+                database=config["database"]
+            )
+            return conn

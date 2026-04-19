59b1aa23547499bc384e0e148441da72521cf3dd
SALAH <salaheddine.rahmani@monborial.ca>
2026-04-18 22:47:04 -0400

    add code to Ajouter_salle Valid

--- services/services_salle.py
+++ services/services_salle.py
@@ -7,3 +7,8 @@ class ServiceSalle:
     def ajouter_salle(self, salle):
         if not all([salle.code, salle.description, salle.categorie, salle.capacite]):
             return False, "All fields are required."
+        if salle.capacite < 1:
+            return False, "invalid capacity"
+
+        self.dao.insert_salle(salle)
+        return True, "Salle add"
